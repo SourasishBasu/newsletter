c858ccabfd72b109019c143fcbab9c1f833b8aa4
swappy <59965507+rycerzes@users.noreply.github.com>
2024-03-08 20:28:12 +0530

    improved zod auth

--- src/validators/auth.ts
+++ src/validators/auth.ts
@@ -1,5 +1,24 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-  email: z.string().email(),
+  email: z
+    .string()
+    .email({ message: "Invalid email address" })
+    .refine(
+      (value) => {
+        const rollno = value.split("@")[0];
+        return /^2\d{6,7}$/.test(rollno);
+      },
+      {
+        message:
+          "The roll number must start with 2 and should be of 7-8 digits.",
+      }
+    )
+    .refine(
+      (value) => {
+        const domain = value.split("@")[1];
+        return domain === "kiit.ac.in";
+      },
+      { message: "The domain must be 'kiit.ac.in'." }
+    ),
 });
