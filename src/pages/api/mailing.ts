8b95aee09a762a371cc211c3bb74eff5857ec1e8
Sasquatch <89185962+SourasishBasu@users.noreply.github.com>
2024-03-18 16:35:54 +0530

    Switched to mailgun.js and added a verification email

--- src/pages/api/mailing.ts
+++ src/pages/api/mailing.ts
@@ -1,10 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import mailgun from "mailgun-js";
-
-const mg = mailgun({
-  apiKey: process.env.MAILGUN_API_KEY || "",
-  domain: process.env.MAILGUN_DOMAIN || "",
-});
+import mailGun from "mailgun.js";
+import formData from "form-data";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
@@ -14,15 +10,39 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(400).json({ message: 'Email and list are required' });
     }
 
-    const data = {
-      subscribed: true,
-      address: email,
-      name: ""
+    const MailGun = new mailGun(formData);
+
+    const mg = MailGun.client({
+      username: "api",
+      key: process.env.MAILGUN_API_KEY,
+    });
+    const DOMAIN = process.env.MAILGUN_DOMAIN;
+    const MAILING_LIST = list;
+    const EMAIL_TO_ADD = email;
+
+    const DATA = {
+      from: `NEWSLETTER MLSA <newsletter@mlsakiit.com>`,
+      to: EMAIL_TO_ADD,
+      subject: "NEWSLETTER SUBSCRIPTION CONFIRMATION",
+      html: `
+            <p style="text-align: center; color: #1a1a1a;">Thank you for subscribing!</p>
+          `,
     };
 
     try {
-      await mg.lists(list).members().create(data);
+
+      // Add email to mailing list
+      await mg.lists.members.createMember(MAILING_LIST, {
+        address: EMAIL_TO_ADD,
+        subscribed: 'yes',
+        upsert: 'yes',
+      });
       res.status(200).json({ message: 'Email added to list successfully' });
+
+      // Send verification email for first time sign-up
+      const mailRes = await mg.messages.create(`${DOMAIN}`, DATA);
+      console.log({ mailRes });
+
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'An error occurred' });
